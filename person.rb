acaa9782899ccd85cf1ad7b8dee589be1502b9f7
ncaskey04 <ncaskey04@gmail.com>
2014-08-13 19:30:59 -0700

    person now has working puke function

--- person.rb
+++ person.rb
@@ -1,4 +1,3 @@
-
 class Person
 
   def initialize(allergies)
@@ -7,37 +6,40 @@ class Person
   end
 
   def eat(food)
-    is_allergic = false
-    food.each do |ingredient|
-      if @allergies.include?(ingredient)
-        is_allergic = true
-        puts "AllergyError"
+    check_food(@allergies, food)
+    puts "Do you want to eat the food?"
+    input = gets.chomp.downcase
+      if input == "yes"
+        puke(food)
+      else puts "You can live"
       end
-      if !is_allergic
-        @stomach.push(food)
-      end
+  end
+
+  def puke(food)
+    @stomach = []
+    puts "BLAARARRRRH"
+  end
+
+end
+
+def check_food(allergies, food)
+  is_allergic = false
+  food.each do |ingredient|
+    if @allergies.include?(ingredient)
+      is_allergic = true
+      puts "Oops! Hey don't eat:( Allergy Error"
     end
   end
+    if !is_allergic
+      @stomach.push(food)
+    end
 end
+
 
 pizza = ["cheese", "gluten", "tomatoes"]
 pan_seared_scallops = ["scallops", "lemons", "pasta", "olive oil"]
 water = ["h", "h", "o"]
 
-bob = Person.new("gluten")
-bob.eat(pizza)
 
-
-
-
-
-
-
-
-# class Food
-#   def initialize(ingredients)
-#     @ingredients = []
-#   end
-#   def cook()
-#   end
-# end
+# bob = Person.new("gluten")
+# bob.eat(pizza)
